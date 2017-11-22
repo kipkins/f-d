737a3d4a1f91c6c601e33773375e3bfa4a37242d
kipkins <kipkins@outlook.com>
2017-11-22 15:20:17 -0600

    changing display attriubte

--- f-d.js
+++ f-d.js
@@ -1,70 +1,51 @@
-// f_n_d
-(function($) {
-    console.log('Pathname', window.location.pathname)
-    $(function() {
-        if (window.location.pathname === '/') {
-            var menu = $('header.Header');
-            var html = menu.html()
-            menu.empty();
-            var intro = $('#intro');
-            intro.append(html);
-            var distance = $('.Header-inner').offset().top;
-            var bottomNavigation = $('.Mobile-bar.Mobile-bar--bottom');
-            bottomNavigation.css({
-                display: 'none'
-            })
-            $window = $(window);
-            $window.scroll(function() { // mobile nav
-                if (window.innerWidth < 1200) {
-                    if (bottomNavigation.css('display').toLowerCase() === 'none') {
-                        bottomNavigation.css({
-                            display: 'block'
-                        })
-                    }
-                    if ($window.scrollTop() === 0) {
-                        bottomNavigation.css({
-                            display: 'none'
-                        })
-                    }
+console.log('Pathname', window.location.pathname)
+$(document).ready(function() {
+    if (window.location.pathname === "/") {
+        var menu = $("header.Header");
+        var html = menu.html();
+        menu.empty();
+        var intro = $("#intro");
+        intro.append(html);
+        var distance = $(".Header-inner").offset().top;
+        var bottomNavigation = $(".Mobile-bar.Mobile-bar--bottom");
+        bottomNavigation.css({
+            display: "none"
+        });
+
+        $(window).scroll(function() { // mobile nav
+            if (window.innerWidth < 1200) {
+                if (bottomNavigation.css("display").toLowerCase() === "none") {
+                    bottomNavigation.removeAttr("style")
+                }
+                if ($(window).scrollTop() === 0) {
+                    bottomNavigation.css({
+                        display: "none"
+                    });
+                }
+            } else {
+                if (distance === 0) {
+                    distance = $(".Header-inner").offset().top;
+                }
+                if ($(window).scrollTop() >= distance) {
+                    $(".Header-inner").remove();
+                    $(menu).append(html);
                 } else {
-                    if (distance === 0) {
-                        distance = $('.Header-inner').offset().top;
-                    }
-                    if ($window.scrollTop() >= distance) {
-                        $('.Header-inner').remove();
-                        $(menu).append(html);
-                    } else {
-                        menu.empty();
-                        var exists = $('.Header-inner');
-                        if (exists.length === 0) {
-                            intro.append(html);
-                        }
+                    menu.empty();
+                    var exists = $(".Header-inner");
+                    if (exists.length === 0) {
+                        intro.append(html);
                     }
                 }
-            })
+            }
+        });
 
-            function setHeight() {
-                windowHeight = $(window).innerHeight();
-                $('#intro').css('min-height', windowHeight);
-            };
-            setHeight();
-
-            $(window).resize(function() {
-                setHeight();
-            });
+        function setHeight() {
+            $("#intro").css("min-height", $(window).innerHeight());
         }
-    });
-}(jQuery));
-//
-// <script type='text/javascript'>
-// var pathname = window.location.pathname;
-// var type = window.location.hash.substr(1);
-// if(type === 'r'){
-// 	pathname = pathname.replace('#r', '');
-//   	window.history.pushState(null,null,pathname);
-// }
-// else {
-//   	window.history.pushState(null,null,pathname += '#r')
-// 	window.location.reload()
-// }
-// </script>
+        setHeight();
+
+        $(window).resize(function() {
+            setHeight();
+        });
+    }
+});
