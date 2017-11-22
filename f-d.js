2699c81e9094a0a67d7a4a9b6aba61dbd733aaa9
Sean O'Donnell <kipkins@outlook.com>
2017-11-21 23:51:59 -0600

    updated lots

--- f-d.js
+++ f-d.js
@@ -1,40 +1,55 @@
-window.onload = function(){
-        var bottomNavigation = $('.Mobile-bar.Mobile-bar--bottom');
-      if(bottomNavigation.length > 0){
-//        var position = bottomNavigation.offset().top;
-          bottomNavigation.css({display: 'none'})
-        $(window).scroll(function(){
-            if(bottomNavigation.css('display').toLowerCase() === 'none'){
-                bottomNavigation.css({display: 'block'})
-            }
-            if($(window).scrollTop() === 0){
-                bottomNavigation.css({display: 'none'})
-            }
-        })
-      }
-      else {
+// fine_n_dandy
+if (window.location.pathname === '/') {
+    $(document).ready(function() {
+        console.log('Pathname', window.location.pathname)
         var menu = $('header.Header');
         var html = menu.html()
         menu.empty();
         var intro = $('#intro');
         intro.append(html);
         var distance = $('.Header-inner').offset().top;
-
+        var bottomNavigation = $('.Mobile-bar.Mobile-bar--bottom');
+        bottomNavigation.css({
+            display: 'none'
+        })
         $window = $(window);
-        $window.scroll(function() {
-          if ( $window.scrollTop() >= distance ) {
-               $('.Header-inner').remove();
-               $(menu).append(html);
-               //$(menu).css({position: 'fixed', top: '0', left: '0', right: '0'})
-          }
-          else {
-
-            menu.empty();
-            var exists = $('.Header-inner');
-            if(exists.length === 0){
-                intro.append(html);
+        $window.scroll(function() { // mobile nav
+            if (window.innerWidth < 1200) {
+                if (bottomNavigation.css('display').toLowerCase() === 'none') {
+                    bottomNavigation.css({
+                        display: 'block'
+                    })
+                }
+                if ($window.scrollTop() === 0) {
+                    bottomNavigation.css({
+                        display: 'none'
+                    })
+                }
+            } else {
+                if (distance === 0) {
+                    distance = $('.Header-inner').offset().top;
+                }
+                if ($window.scrollTop() >= distance) {
+                    $('.Header-inner').remove();
+                    $(menu).append(html);
+                } else {
+                    menu.empty();
+                    var exists = $('.Header-inner');
+                    if (exists.length === 0) {
+                        intro.append(html);
+                    }
+                }
             }
-          }
+        })
+
+        function setHeight() {
+            windowHeight = $(window).innerHeight();
+            $('#intro').css('min-height', windowHeight);
+        };
+        setHeight();
+
+        $(window).resize(function() {
+            setHeight();
         });
-   }
+    });
 }
