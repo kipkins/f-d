ded32455f0ec2675fcb0abdf10f1abf811f84821
kipkins <kipkins@outlook.com>
2017-11-22 22:06:26 -0600

    try again

--- f-d.js
+++ f-d.js
@@ -1,48 +1,51 @@
 $(document).ready(function() {
-  var menu = $('header.Header');
-  var html = menu.html();
-  var intro = $('#intro');
-  $window = $(window);
-  var distance = $('.Header-inner').offset().top;
-  var bottomNavigation = $('.Mobile-bar.Mobile-bar--bottom');
-
+  var menu = $("header.Header");
+  var menuHtml = menu.html();
+  var intro = $("#intro");
+  var bottomNavigation = $(".Mobile-bar.Mobile-bar--bottom");
 
   if(window.location.pathname === "/"){
+    menu.empty();
+    intro.append(menuHtml);
+    var distance = $(".Header-inner").offset().top;
     bottomNavigation.css({
-        display: 'none'
-    })
+        display: "none"
+    });
     $(window).scroll(function() {
-    if(window.innerWidth < 1200) {
-      mobileScreenTricks();
-    }
-    else fullScreenTricks();
+      if(window.innerWidth < 1200) {
+        mobileScreenTricks();
+      }
+      else{
+        fullScreenTricks();
+      }
     });
   }
 
   function mobileScreenTricks(){
-    if (bottomNavigation.css('display').toLowerCase() === 'none') {
+    if (bottomNavigation.css("display".toLowerCase() === "none") {
         bottomNavigation.removeAttr("style");
     }
     if ($window.scrollTop() === 0) {
         bottomNavigation.css({
-            display: 'none'
+            display: "none"
         })
     }
   }
 
   function fullScreenTricks(){
     if (distance === 0) {
-        distance = $('.Header-inner').offset().top;
+      distance = $(".Header-inner").offset().top;
     }
     if ($window.scrollTop() >= distance) {
-        $('.Header-inner').remove();
-        $(menu).append(html);
-    } else {
-        menu.empty();
-        var exists = $('.Header-inner');
-        if (exists.length === 0) {
-            intro.append(html);
-        }
+      $(".Header-inner").remove();
+      $(menu).append(menuHtml);
+    }
+    else {
+      menu.empty();
+      var exists = $(".Header-inner");
+      if (exists.length === 0) {
+          intro.append(menuHtml);
+      }
     }
   }
 
