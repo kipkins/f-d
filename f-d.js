6a442f4a8cff1afa35975d0583dd4cbc6fceb945
kipkins <kipkins@outlook.com>
2017-11-22 22:30:45 -0600

    backing it up

--- f-d.js
+++ f-d.js
@@ -1,52 +1,57 @@
-$(document).ready(function() {
-  var menu = $("header.Header");
-  var menuHtml = menu.html();
-  var intro = $("#intro");
-  var bottomNavigation = $(".Mobile-bar.Mobile-bar--bottom");
+// f_n_d
+(function($) {
+    console.log('Pathname', window.location.pathname)
+    $(function() {
+        if (window.location.pathname === '/') {
+            var menu = $('header.Header');
+            var html = menu.html()
+            menu.empty();
+            var intro = $('#intro');
+            intro.append(html);
+            var distance = $('.Header-inner').offset().top;
+            var bottomNavigation = $('.Mobile-bar.Mobile-bar--bottom');
+            bottomNavigation.css({
+                display: 'none'
+            })
+            $window = $(window);
+            $window.scroll(function() { // mobile nav
+                if (window.innerWidth < 1200) {
+                    if (bottomNavigation.css('display').toLowerCase() === 'none') {
+                        bottomNavigation.css({
+                            display: 'block'
+                        })
+                    }
+                    if ($window.scrollTop() === 0) {
+                        bottomNavigation.css({
+                            display: 'none'
+                        })
+                    }
+                } else {
+                    if (distance === 0) {
+                        distance = $('.Header-inner').offset().top;
+                    }
+                    if ($window.scrollTop() >= distance) {
+                        $('.Header-inner').remove();
+                        $(menu).append(html);
+                    } else {
+                        menu.empty();
+                        var exists = $('.Header-inner');
+                        if (exists.length === 0) {
+                            intro.append(html);
+                        }
+                    }
+                }
+            })
 
-  if(window.location.pathname === "/"){
-    menu.empty();
-    intro.append(menuHtml);
-    var distance = $(".Header-inner").offset().top;
-    bottomNavigation.css({
-        display: "none"
+            function setHeight() {
+                windowHeight = $(window).innerHeight();
+                $('#intro').css('min-height', windowHeight);
+            };
+            setHeight();
+
+            $(window).resize(function() {
+                setHeight();
+            });
+        }
     });
-    $(window).scroll(function() {
-      if(window.innerWidth < 1200) {
-        mobileScreenTricks();
-      }
-      else{
-        fullScreenTricks();
-      }
-    });
-  }
-
-  function mobileScreenTricks(){
-    if (bottomNavigation.css("display".toLowerCase() === "none")) {
-        bottomNavigation.removeAttr("style");
-    }
-    if ($window.scrollTop() === 0) {
-        bottomNavigation.css({
-            display: "none"
-        })
-    }
-  }
-
-  function fullScreenTricks(){
-    if (distance === 0) {
-      distance = $(".Header-inner").offset().top;
-    }
-    if ($window.scrollTop() >= distance) {
-      $(".Header-inner").remove();
-      $(menu).append(menuHtml);
-    }
-    else {
-      menu.empty();
-      var exists = $(".Header-inner");
-      if (exists.length === 0) {
-          intro.append(menuHtml);
-      }
-    }
-  }
-
-});
+}(jQuery));
